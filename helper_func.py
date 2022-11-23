5b52d3de5dbdc8e24d8ff427ab867d722c88bc16
mathislindner <mathis.lindner@gmail.com>
2022-11-23 19:14:32 +0100

    working on concat of new DS

--- helper_func.py
+++ helper_func.py
@@ -12,7 +12,7 @@ import random
 def test_model(model, n, test_loader):
     model.eval()
     correct = 0
-    for x_, y_, _ in test_loader:
+    for x_, y_ in test_loader:
         x_ = x_.view(-1, 28 * 28)
         x_, y_ = Variable(x_), Variable(y_)
         output = model(x_)
@@ -22,41 +22,48 @@ def test_model(model, n, test_loader):
     print('Test Accuracy of the model on the {} test images: {} %'.format(n, 100 * correct / len(test_loader.dataset)))
     return accuracy
 
-def get_data_loaders(n, batch_size):
+def get_data_loaders(n, batch_size, transform):
     random.seed(42)
     # randomly select 2 out of 10 classes
     classes = random.sample(range(10), 10)
-    classes_train = classes[2*n:2*n+2]
-    classes_test = classes[:2*n+2]
-
-    transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-    ])
+    classes_train = classes[2*n:2*n+2] # 2 classes for training
+    classes_test = classes[:2*n+2] #all classes seen so far
 
     MNIST_train_full = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
     MNIST_test_full = datasets.MNIST(root='./data', train=False, transform=transform, download=True)
-    MNIST_train_n = torch.utils.data.Subset(MNIST_train_full, [i for i, (x, y) in enumerate(MNIST_train_full) if y in classes_train])
-    MNIST_test_n = torch.utils.data.Subset(MNIST_test_full, [i for i, (x, y) in enumerate(MNIST_test_full) if y in classes_test])
 
-    train_loader = torch.utils.data.DataLoader(MNIST_train_n, batch_size=batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(MNIST_test_n, batch_size=batch_size, shuffle=True)
+    #get indices of the classes we want
+    train_indices = [i for i, x in enumerate(MNIST_train_full.targets) if x in classes_train]
+    test_indices = [i for i, x in enumerate(MNIST_test_full.targets) if x in classes_test]
+
+    #create new datasets with only the classes we want
+    MNIST_train = torch.utils.data.Subset(MNIST_train_full, train_indices)
+    MNIST_test = torch.utils.data.Subset(MNIST_test_full, test_indices)
+
+    # Data Loader (Input Pipeline)
+    train_loader = torch.utils.data.DataLoader(dataset=MNIST_train, batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(dataset=MNIST_test, batch_size=batch_size, shuffle=False)
+    
     return train_loader, test_loader
 
-def generate_data(G, D, n_images):
+def generate_data(G, D, n_images, batch_size = 128):
+    #create fake images, label them and return them as a dataset of tensors
+    loops = int(n_images/batch_size)
     G.eval()
-    fixed_z_ = torch.randn((n_images, 100))    # fixed noise
-    test_images = G(fixed_z_)
-    test_images = test_images.view(test_images.size(0), 1, 28, 28)
-    test_images = test_images.data
-
     D.eval()
-    test_images = test_images.view(-1, 28 * 28)
-    test_images = Variable(test_images)
-    output = D(test_images)
-    pred = output.data.max(1, keepdim=True)[1]
-    dataset = torch.utils.data.TensorDataset(test_images, pred)
-    return dataset
+    fake_images = []
+    fake_labels = []
+    for i in range(loops):
+        z_ = torch.randn((batch_size, 100))
+        fake_images.append(G(z_).reshape(batch_size, 28*28))
+        fake_labels.append(D(fake_images[i]))
+    fake_images = torch.reshape(torch.cat(fake_images), (-1, 1, 28, 28))
+    fake_labels = torch.Tensor(torch.cat(fake_labels)).reshape(-1, 1)
+
+    #create dataset
+    fake_dataset = torch.utils.data.TensorDataset(fake_images, fake_labels)
+    return fake_dataset
+
 
 def get_accuracies(evaluations):
     accuracies = []
@@ -88,18 +95,12 @@ def train_GAN(G, D, train_loader, G_optimizer, D_optimizer, train_epoch):
     fixed_z_ = torch.randn((5 * 5, 100))    # fixed noise
     fixed_z_ = Variable(fixed_z_, volatile=True)
 
-    # data_loader
-    transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-    ])
-
     # Binary Cross Entropy loss
     BCE_loss = nn.BCELoss()
     for epoch in range(train_epoch):
         D_losses = []
         G_losses = []
-        for x_, _, _ in tqdm(train_loader):
+        for x_, _ in train_loader:
             D.zero_grad()
             # convert x_ to tensor
             #x_ = torch.tensor(x_).view(-1, 28 * 28)
